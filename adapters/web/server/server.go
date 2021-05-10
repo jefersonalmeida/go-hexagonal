eb24c3de6895789a2eec4b9fd69f341172713ed1
Jeferson Almeida <jeferson.almeida@outlook.com>
2021-05-09 21:25:04 -0400

    Cria server

---
+++ adapters/web/server/server.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/jefersonalmeida/go-hexagonal/adapters/web/handler"
+	"github.com/jefersonalmeida/go-hexagonal/application"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type WebServer struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebserver() *WebServer {
+	return &WebServer{}
+}
+
+func (w WebServer) Server() {
+
+	r := mux.NewRouter()
+	n := negroni.New(
+		negroni.NewLogger(),
+	)
+
+	handler.MakeProductHandlers(r, n, w.Service)
+	http.Handle("/", r)
+
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		Addr:              ":9000",
+		Handler:           http.DefaultServeMux,
+		ErrorLog:          log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
